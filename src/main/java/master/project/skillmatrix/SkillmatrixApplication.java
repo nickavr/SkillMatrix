06331833dcbd5d59b599e700f438b3c3fd7b4046
navram <navram@murex.com>
2023-03-22 10:55:01 +0200

    first commit - project setup

---
+++ src/main/java/master/project/skillmatrix/SkillmatrixApplication.java
@@ -0,0 +1,13 @@
+package master.project.skillmatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillmatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillmatrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/master/project/skillmatrix/SkillmatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package master.project.skillmatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillmatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
